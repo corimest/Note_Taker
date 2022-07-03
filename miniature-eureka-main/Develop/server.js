9b153e5f590a22f5675ad8ae0246232a56a55e2d
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-07-03 11:51:34 -0500

    new note posting and added to json functionality

--- miniature-eureka-main/Develop/server.js
+++ miniature-eureka-main/Develop/server.js
@@ -1,20 +1,60 @@
+//required to write new notes to json data
+const fs = require('fs'); 
+const path = require('path'); 
 //require express
 const express = require('express'); 
 // instantiate the server
 const app = express(); 
 // require notes data
-const notes = require('./db/db.json'); 
+const { notes } = require('./db/db.json'); 
 
 const PORT = process.env.PORT || 3001
 
+//middlewarae
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
 
+//create new note function 
+function createNote (body, notes) {
+    const note = body;  
+    notes.push(note); 
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'), 
+        JSON.stringify({ notes: notes }, null, 2)
+    ); 
+    return note; 
+}
+
+//Validate Data
+function validateNote (note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false; 
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false; 
+    }
+    return true; 
+}
 
 
 app.get('/api/notes', (req, res) => {
-    res.json('notes'); 
+    res.json(notes); 
 }); 
 
+app.post('/api/notes', (req, res) => {
+    //set id based on what the next index of the array will be 
+    req.body.id = notes.length.toString(); 
 
+    //if data not validated, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.')
+    } else{
+        //add note to json file and notes array
+        const note = createNote(req.body, notes); 
+
+        res.json(note); 
+    }
+});
 
 
 // Listen for requests method
